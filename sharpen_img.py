e19066dffc0f07c7f7724d00a1a75e740e6a9677
Alex Gaudio <adgaudio@gmail.com>
2019-11-08 20:24:23 -0500

    sharpen supports different shapes for t.  incorporates background too

--- sharpen_img.py
+++ sharpen_img.py
@@ -5,10 +5,15 @@ from idrid import IDRiD
 from matplotlib import pyplot as plt
 
 
-def sharpen(img, t=0.1, blur_radius=20, blur_guided_eps=1e-2):
-    """Use distortion model to deblur image.  Equivalent to
-    1/t * img - (1-1/t) * blurry(img).
+def sharpen(img, bg, t=0.15, blur_radius=30, blur_guided_eps=1e-2):
+    """Use distortion model to deblur image.  Equivalent to usharp mask.
+
+        1/t * img - (1-1/t) * blurry(img).
+
     Then, apply guided filter to smooth result but preserve edges.
+
+    img - image to sharpen.
+    bg - image background
     """
     # blurring (faster than ndi.gaussian_filter(I)
     A = cv2.ximgproc.guidedFilter(
@@ -16,21 +21,27 @@ def sharpen(img, t=0.1, blur_radius=20, blur_guided_eps=1e-2):
         img.astype('float32'),
         img.astype('float32'),
         blur_radius, blur_guided_eps)
-    A[bg] = 0
+    if bg is not None:
+        A[bg] = 0
 
     #  t_refined = np.ones(img.shape[:2]) * t
-    t_refined = t
 
+    if np.shape(t):
+        t_refined = np.expand_dims(t, -1).astype('float')
+        t_refined[bg] = 1  # division by zero
+    else:
+        t_refined = t
     J = (  # Eq. 22 of paper
-        img.astype('float')-A) \
-        / np.expand_dims(t_refined, -1).astype('float') \
-        + A
+        img.astype('float')-A) / t_refined + A
+    if bg is not None:
+        J[bg] = 0
 
     r2 = cv2.ximgproc.guidedFilter(
         img.astype('float32'),
         J.astype('float32'),
         2, 1e-2)
-    r2[bg] = 0
+    if bg is not None:
+        r2[bg] = 0
     return r2
 
 
@@ -47,30 +58,30 @@ if __name__ == "__main__":
     img[bg] = 0
 
 
-    J = sharpen(img)
-    r2 = cv2.ximgproc.guidedFilter(
-        img.astype('float32'),
-        J.astype('float32'),
-        2, 1e-2)
-    r2[bg] = 0
+    J = sharpen(img, bg)
+    #  r2 = cv2.ximgproc.guidedFilter(
+        #  img.astype('float32'),
+        #  J.astype('float32'),
+        #  2, 1e-2)
+    #  r2[bg] = 0
 
 
     plt.figure(num=1) ; plt.imshow(img)
     plt.figure(num=2) ; plt.imshow(util.norm01(J, None))
     plt.figure(num=3) ; plt.imshow(J.clip(0, 1))
-    plt.figure(num=4) ; plt.imshow(util.norm01(r2, bg))
-    plt.figure(num=5) ; plt.imshow(r2.clip(0, 1))
+    #  plt.figure(num=4) ; plt.imshow(util.norm01(r2, bg))
+    #  plt.figure(num=5) ; plt.imshow(r2.clip(0, 1))
 
-    import evaluation as EV
-    import metric
+    #  import evaluation as EV
+    #  import metric
 
-    EV.empirical_evaluation(
-        {'img': img,
-        'radiance': J,
-        'r2': r2,
-        'norm01': util.norm01(J, bg),
-        'clip': J.clip(0, 1),
-        'r2norm01': util.norm01(r2, bg),
-        'r2clip': r2.clip(0, 1),
-        },
-        labels, metric.eval_methods, bg, num_procs=8)
+    #  EV.empirical_evaluation(
+        #  {'img': img,
+        #  'radiance': J,
+        #  'r2': r2,
+        #  'norm01': util.norm01(J, bg),
+        #  'clip': J.clip(0, 1),
+        #  'r2norm01': util.norm01(r2, bg),
+        #  'r2clip': r2.clip(0, 1),
+        #  },
+        #  labels, metric.eval_methods, bg, num_procs=8)
