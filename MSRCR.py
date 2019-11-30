80e17889e6341b3333ef59c5959e4094346ec2dd
Alex Gaudio <adgaudio@gmail.com>
2019-11-30 17:19:52 -0500

    modify MSRCR to 1) blur with a guidedFilter, which is faster and has slightly different results 2) work fully with masked arrays

--- MSRCR.py
+++ MSRCR.py
@@ -1,10 +1,11 @@
 """
 Retinex image enhancement method.  Retinex is equivalent to inverted dehazing.
 Code downloaded from https://github.com/falrom/MSRCR_Python/blob/master/MSRCR.py
-on 2019-11-01
+on 2019-11-01 and slightly modified.
 """
 from matplotlib import pyplot as plt
 import numpy as np
+import scipy.ndimage as ndi
 import argparse
 import cv2
 import os
@@ -29,12 +30,21 @@ def retinex_scales_distribution(max_scale, nscales):
 
 def CR(im_ori, im_log, alpha=128., gain=1., offset=0.):
     im_cr = im_log * gain * (
-            np.log(alpha * (im_ori + 1.0)) - np.log(np.sum(im_ori, axis=2) + 3.0)[:, :, np.newaxis]) + offset
+            np.ma.log(alpha * (im_ori + 1.0)) - np.ma.log(np.ma.sum(im_ori, axis=2) + 3.0)[:, :, np.newaxis]) + offset
     return im_cr
 
 
-def MSRCR(image_path, max_scale, nscales, dynamic=2.0, do_CR=True):
-    im_ori = np.float32(cv2.imread(image_path)[:, :, (2, 1, 0)])
+def blur(img_1channel, sigma):
+    #  rv = ndi.gaussian_filter(img_1channel, sigma)
+    # faster blur
+    img = (img_1channel/255).astype('float32')
+    rv = cv2.ximgproc.guidedFilter(img, img, int(sigma), 1e-2) * 255
+    return rv
+
+
+def MSRCR(rgb_img, max_scale, nscales, dynamic=2.0, do_CR=True):
+    assert rgb_img.max() > 1
+    im_ori = rgb_img
     scales = retinex_scales_distribution(max_scale, nscales)
 
     im_blur = np.zeros([len(scales), im_ori.shape[0], im_ori.shape[1], im_ori.shape[2]])
@@ -43,21 +53,22 @@ def MSRCR(image_path, max_scale, nscales, dynamic=2.0, do_CR=True):
     for channel in range(3):
         for s, scale in enumerate(scales):
             # If sigma==0, it will be automatically calculated based on scale
-            im_blur[s, :, :, channel] = cv2.GaussianBlur(im_ori[:, :, channel], (0, 0), scale)
-            im_mlog[s, :, :, channel] = np.log(im_ori[:, :, channel] + 1.) - np.log(im_blur[s, :, :, channel] + 1.)
+            im_blur[s, :, :, channel] = blur(im_ori[:, :, channel], scale)
+            im_mlog[s, :, :, channel] = np.ma.log(im_ori[:, :, channel] + 1.) - np.log(im_blur[s, :, :, channel] + 1.)
 
     im_retinex = np.mean(im_mlog, 0)
+    im_retinex = np.ma.masked_array(im_retinex, im_ori.mask)
     if do_CR:
         im_retinex = CR(im_ori, im_retinex)
 
-    im_rtx_mean = np.mean(im_retinex)
-    im_rtx_std = np.std(im_retinex)
+    im_rtx_mean = np.ma.mean(im_retinex)
+    im_rtx_std = np.ma.std(im_retinex)
     im_rtx_min = im_rtx_mean - dynamic * im_rtx_std
     im_rtx_max = im_rtx_mean + dynamic * im_rtx_std
 
     im_rtx_range = im_rtx_max - im_rtx_min
 
-    im_out = np.uint8(np.clip((im_retinex - im_rtx_min) / im_rtx_range * 255.0, 0, 255))
+    im_out = np.uint8(np.ma.clip((im_retinex - im_rtx_min) / im_rtx_range * 255.0, 0, 255))
 
     return im_out
 
@@ -73,5 +84,8 @@ if __name__ == '__main__':
     # plt.show()
     ####################################################################################
     args = parser.parse_args()
-    im_out = MSRCR(args.input, args.s, args.n, args.d, not args.no_cr)
+    import util
+    im_in = plt.imread(args.input).copy().astype('float32')
+    im_in = np.ma.masked_array(im_in, (util.get_background(im_in)))
+    im_out = MSRCR(im_in, args.s, args.n, args.d, not args.no_cr)
     cv2.imwrite(args.output, im_out[:, :, (2, 1, 0)])
