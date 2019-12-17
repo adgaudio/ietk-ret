e0e4fb2d00201bb03c53804675f56b025c720b08
Alex Gaudio <adgaudio@gmail.com>
2019-12-17 16:24:36 -0500

    adds setup.py adds MIT license updates gitignore

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+from setuptools import setup
+
+
+setup(
+    name='IETK',
+    version='0.01',
+    description='Image Enhancement Toolkit IETK',
+    author='Alex Gaudio',
+    packages=['ietk']
+)
