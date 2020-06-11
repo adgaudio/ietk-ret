91f1559d3201b5804dc30a5e3448ed654aa10110
Alex Gaudio <adgaudio@gmail.com>
2020-06-10 23:51:09 -0400

    Bump version: 0.0.1 → 0.0.2

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ README = (HERE / "README.md").read_text()
 
 setup(
     name='IETK-Ret',
-    version='0.0.1',
+    version='0.0.2',
     description='Image Enhancement Toolkit for Retinal Fundus Images IETK-Ret',
     long_description=README,
     long_description_content_type="text/markdown",
